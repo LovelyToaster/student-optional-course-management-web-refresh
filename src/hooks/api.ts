bb3e774592627eb5a01e748885696e1e252373bb
LovelyToaster94 <LovelyToaster94@outlook.com>
2025-05-11 14:32:22 +0800

    feat(api): 完善登录状态拦截处理

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import code from "./code";
 import {notification} from "ant-design-vue";
 
+let isFailed = false
 const apiInstance = axios.create({
     baseURL: 'http://localhost:8080',
 });
@@ -9,13 +10,29 @@ apiInstance.defaults.headers.common["Content-Type"] = "application/x-www-form-ur
 apiInstance.defaults.withCredentials = true
 apiInstance.interceptors.response.use(
     (response) => {
-        if (response.data.code === code.LOGIN_FAILED && window.location.pathname != "/login") {
+        if (response.data.code === code.TOKEN_EXPIRED && !isFailed) {
             notification.error({
                 message: "失败",
                 description: response.data.message,
             });
-            window.location.pathname = "/login"
+            isFailed = true
+            setTimeout(() => {
+                window.location.pathname = '/login'
+            }, 3000)
+            return Promise.reject(new Error("token expired"))
         }
+        if (response.data.code === code.NO_LOGIN && window.location.pathname !== "/login" && !isFailed) {
+            notification.error({
+                message: "失败",
+                description: response.data.message,
+            });
+            isFailed = true
+            setTimeout(() => {
+                window.location.pathname = '/login'
+            }, 3000)
+            return Promise.reject(new Error("no login"))
+        }
+        isFailed = false
         return response;
     },
     (error) => {
